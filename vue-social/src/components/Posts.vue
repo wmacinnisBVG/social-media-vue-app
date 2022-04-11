cf1ffeb3735fadb89cd36a81f4541ca332a05dda
William MacInnis <wmacinnis@bayviewglen.ca>
2022-04-11 13:39:47 -0400

    Init commit

---
+++ vue-social/src/components/Posts.vue
@@ -0,0 +1,40 @@
+<template>
+  <div  :key="post.id" v-for="post in posts">
+  <p>{{ post.id }}}</p>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      posts: []
+    }
+  },
+  name: 'Posts',
+  components: {},
+  methods: {
+    async fetchPosts() {
+      const res = await fetch('api/posts')
+      const data = await res.json()
+      console.log('test');
+      return data
+    },
+  /**
+    async deleteContact(id) {
+      if (confirm('Are you sure?')) {
+        const res = await fetch(`api/contacts/${id}`, {
+          method: 'DELETE',
+
+        })
+        res.status === 200 ? (this.tasks = this.tasks.filter((task) => task.id !== id)) : alert('Error deleting task')
+
+
+      }
+    }
+    */
+  },
+  async created() {
+    this.posts = await this.fetchPosts()
+  }
+}
+</script>
---
+++ vue-social/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Vue Social</h1>
+  <Posts />
+</template>
+<script>
+import Posts from '@/components/Posts.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Posts
+  }
+}
+</script>
